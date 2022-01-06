c2ffc517f24c02fd2b957ef6bfb5eeb935487b65
nishanta1234 <nishantapaudel29@gmail.com>
2022-01-06 16:28:32 +0545

    Lab 2

---
+++ Lab/Lab 2/Userdefine package/Demo.java
@@ -0,0 +1,8 @@
+import com.gces.nishanta.PackageTry;
+public class Demo {
+    public static void main(String[] args) {
+        PackageTry demo = new PackageTry();
+        demo.showMessage();
+    }
+    
+}
